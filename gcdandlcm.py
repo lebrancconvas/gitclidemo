f80c5799d664c20d09f81b2ef52eb1ee18a5a932
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-07 05:53:01 +0700

    Initial Commit.

---
+++ gcdandlcm.py
@@ -0,0 +1,13 @@
+# Finding GCD (Greatest Common Divisor)
+def gcd(num1,num2):
+    if(num1 == num2):
+        return num1
+    if(num1 > num2):
+        return gcd(num1-num2,num2)
+    return gcd(num1,num2-num1)
+
+# Finding LCM (Lowest Common Multiple)
+def lcm(num1,num2):
+    return (num1*num2) / gcd(num1,num2)
+
+# This is git for terminal demo.
